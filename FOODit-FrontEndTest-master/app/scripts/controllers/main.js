2752d02c618811d709c5475024e268119c1ba4e4
Julia Romer <jromer.london@gmail.com>
2016-01-18 10:34:44 +0000

    prepare new tags arrays

--- FOODit-FrontEndTest-master/app/scripts/controllers/main.js
+++ FOODit-FrontEndTest-master/app/scripts/controllers/main.js
@@ -9,9 +9,28 @@
  */
 angular.module('jstestApp')
   .controller('MainCtrl', ['$scope', 'MenuService', function ($scope, MenuService) {
-	$scope.menu = {};
+		$scope.menu = {};
     MenuService.get('/data/menu.json').success(function(data) {
-	  $scope.menu = data;
+	  //$scope.menu = data;
+
+	  /*
+		var menuobj = data;
+	  for (var i=0; i<menuobj.meals.length; i++){
+	  	//create alternative array within every meals loop
+			var othertags = []; 
+			//loop through every tag of each meal
+	  	for (var j=0, tagz=menuobj.meals[i].tags; j<tagz.length; j++){
+	  		//if tag starts with a hash, push into alternative array
+	  		if (tagz[i].charAt(0)==="#"){
+	  			othertags.push((tagz.splice(i,1))[0])//[0] bc returns array?
+	  		}
+	  	}
+	  	//log arrays of 1st meal to check contents
+	  	console.log("tags array: " + menuobj.meals[0].tags);
+	  	console.log("othertags array: " + menuobj.meals[0].tags);	  	
+	  };
+	  */
+
 	});
 
   }
