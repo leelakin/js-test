7e55cd2ef22edbf01649db89d421ddc0aee77970
Julia Romer <jromer.london@gmail.com>
2016-01-18 22:06:06 +0000

    added basket functionality

--- FOODit-FrontEndTest-master/app/scripts/controllers/main.js
+++ FOODit-FrontEndTest-master/app/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('jstestApp')
   .controller('MainCtrl', ['$scope', 'MenuService', function ($scope, MenuService) {
 		$scope.menu = {};
     MenuService.get('/data/menu.json').success(function(data) {
-	  $scope.menu = data;
+	  //$scope.menu = data;
 	  //console.log(data.meals[0].tags);
 	  
 		var menuobj = data;
@@ -22,7 +22,6 @@ angular.module('jstestApp')
 	  	for (var j=0, tagz=menuobj.meals[i].tags; j<tagz.length; j++){
 	  		//if tag starts with a hash, push into alternative array
 				
-	  		console.log("tag array before splicing: " + tagz);
 	  		if (tagz[j].charAt(0)==="#"){
 	  			var splicedtag = (tagz.splice(j,1))[0];
 
@@ -31,13 +30,26 @@ angular.module('jstestApp')
 	  			j = j-1;
 	  		}
 	  	}
-	  	//check arrays of this round
-	  	console.log("tags array: " + menuobj.meals[i].tags);
-	  	console.log("othertags array: " + menuobj.meals[i].othertags);
-	  	console.log("meal loop done.");	  	
 	  };
 	  $scope.menu = menuobj;
-	  
+
+	  $scope.totalPrice = 0;
+	  $scope.mainCount = 0;
+	  $scope.basket = function(meal){
+	  	console.log("basket " + meal);
+	  	var $basket = $(".basketbox");
+	  	//check whether basket is still hidden - if yes, show it
+	  	if(!$basket.hasClass("basketon")){
+		  	$basket.show().addClass("basketon");
+		  	$(".header").find(".first").hide();
+		  };
+	  	//add meal.name to dropdown and meal.price to total
+		  $scope.mainCount += 1;
+		  $scope.totalPrice += +meal.price;
+		 	//insert new values
+		 	$basket.find(".counthere").text($scope.mainCount);
+		 	$basket.find(".pricehere").text(($scope.totalPrice).toFixed(2));
+	  };
 
 	});
 
