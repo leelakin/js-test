f9557d9b729eaad089853e936af027fd649e07c8
Julia Romer <jromer.london@gmail.com>
2016-01-19 00:10:31 +0000

    added basket dropdown & meal list with prices

--- FOODit-FrontEndTest-master/app/scripts/controllers/main.js
+++ FOODit-FrontEndTest-master/app/scripts/controllers/main.js
@@ -33,23 +33,41 @@ angular.module('jstestApp')
 	  };
 	  $scope.menu = menuobj;
 
+	  var $basket = $(".basketbox");
+	  var $bascontent = $basket.closest(".header").find(".basketcontent");
 	  $scope.totalPrice = 0;
 	  $scope.mainCount = 0;
 	  $scope.basket = function(meal){
 	  	console.log("basket " + meal);
-	  	var $basket = $(".basketbox");
 	  	//check whether basket is still hidden - if yes, show it
 	  	if(!$basket.hasClass("basketon")){
 		  	$basket.show().addClass("basketon");
 		  	$(".header").find(".first").hide();
 		  };
-	  	//add meal.name to dropdown and meal.price to total
+	  	//add meal.price to total
 		  $scope.mainCount += 1;
 		  $scope.totalPrice += +meal.price;
 		 	//insert new values
 		 	$basket.find(".counthere").text($scope.mainCount);
 		 	$basket.find(".pricehere").text(($scope.totalPrice).toFixed(2));
+		 	//add meal.name to dropdown
+		 	var $newmeal = $("<p class=\"mealitem\">" +meal.name+ " || £" + meal.price+ "</p>");
+		 	$bascontent.append($newmeal);
 	  };
+	  $scope.basketToggle = function(){
+	  	console.log("enter basketToggle function");
+	  	if(!$bascontent.hasClass("basketlist_open")){
+	  		$bascontent.addClass("basketlist_open").slideDown();
+	  		console.log("now has basketlist_open tag? " + $basket.hasClass("basketlist_open"));
+	  		$basket.find(".mainscount").hide();
+	  		$basket.find(".btn").show();
+	  	}else{
+	  		$bascontent.removeClass("basketlist_open").slideUp();
+	  		console.log("now has basketlist_open tag? " + $basket.hasClass("basketlist_open"));
+	  		$basket.find(".mainscount").show();
+	  		$basket.find(".btn").hide();
+	  	}
+	  }
 
 	});
 
